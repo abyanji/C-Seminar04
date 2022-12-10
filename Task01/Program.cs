5f22d3f524c2683ae47cef2afddee7181694f513
abyanji <a.byanjil99@gmail.com>
2022-12-10 22:19:17 +0300

    Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,21 @@
+﻿// double i = 3.45666;
+// i = Math.Round(i, 2);
+// Console.WriteLine(i);
+
+Console.Clear();
+Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine());
+
+int Sum(int number)
+{
+    int i;
+    int sum = 0;
+    for (i = 1; i <= number; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+int answer = Sum(number);
+Console.WriteLine(answer);
