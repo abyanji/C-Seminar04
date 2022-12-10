c1d19bfd4b69a3a9520b363988a474ed2554dd58
abyanji <a.byanjil99@gmail.com>
2022-12-11 00:02:51 +0300

    Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+int DataEntry(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int[] FillArray(int[] array)
+{
+    int length = array.Length;
+    int i;
+    for (i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+    return array;
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int i;
+    for (i = 0; i < count; i++)
+    {
+        Console.Write($"{col[i]} ");
+    }
+}
+
+int arraySize = DataEntry("Введите размер массива: ");
+int[] array = new int[arraySize];
+array = FillArray(array);
+PrintArray(array);
