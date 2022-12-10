7bcba855820d706235656c3a34f2d55c995b8f47
abyanji <a.byanjil99@gmail.com>
2022-12-10 22:40:29 +0300

    Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+int DataEntryXY(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int AmountOfNumbers(int number)
+{
+    int amount = 0;
+    for (amount = 0; number > 0; amount++)
+    {
+        number = number / 10;
+    }
+    return amount;
+}
+
+int number = DataEntryXY("Введите число ");
+int amount = AmountOfNumbers(number);
+Console.WriteLine($"В числе {number} -> {amount} числа");
