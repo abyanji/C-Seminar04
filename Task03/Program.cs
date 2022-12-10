89e7035300dcee2277c4a37871641077f780255c
abyanji <a.byanjil99@gmail.com>
2022-12-10 23:10:50 +0300

    Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+int DataEntry(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+int ProductOfNumbers(int number)
+{
+    int amount;
+    int product = 1;
+    for (amount = 1; amount <= number; amount++)
+    {
+        product = product * amount;
+    }
+    return product;
+}
+
+int number = DataEntry("Введите число ");
+int product = ProductOfNumbers(number);
+Console.WriteLine($"Произведение всех числел до {number} -> {product}");
